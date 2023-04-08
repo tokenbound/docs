aafb8ed220af6ea1d9351631bb6decc0e204ba95
druzydowns <druzydowns@gmail.com>
2023-04-08 13:58:02 -0400

    initialize Nextra, create basic documentation page structure

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+//module.exports = nextConfig;
+module.exports = withNextra();
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
